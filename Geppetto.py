91c5a8aea31c64b56746ad815cba1c9862d44a7a
Yehonatan Buchnik <yontyon@gmail.com>
2019-07-04 13:49:44 +0300

    add dev tools

--- Geppetto.py
+++ Geppetto.py
@@ -1,13 +1,7 @@
-import subprocess
-from sys import stdout
+#!/usr/bin/python3.7
 
 import cli
 
 if __name__ == '__main__':
     cli.main()
-    # while True:
-    #     cmd = input('gep-cli> ')
-    #     if cmd == 'exit':
-    #         print('Goodbye')
-    #         break
-    #     subprocess.call(['python', 'cli.py', cmd], stdout=stdout)
+
